0d5331abc73ab8e47a7f2ef2840263b8be81d9a8
Oighty <oighty6@gmail.com>
2023-08-02 11:59:52 -0500

    feat: add example exercise widget (not fully complete)

--- examples/wagmi-example/src/App.tsx
+++ examples/wagmi-example/src/App.tsx
@@ -2,13 +2,15 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 import { GetOLMPricing } from './components/GetOLMPricing'
 import { ExerciseWidget } from './components/ExerciseWidget'
+import {usePublicClient} from 'wagmi'
+import {goerli} from "wagmi/chains";
+import {useState} from 'react'
 
 export function App() {
   const { isConnected } = useAccount()
 
-  const oTokens = [
-    
-  ];
+  const publicClient = usePublicClient(goerli);
+  const [address, setAddress] = useState<`0x${string}`>("0xb9fa19fc77fab92d90b0a010fbe7b22b045e5dd9");
 
   return (
     <div className="prose">
@@ -19,11 +21,20 @@ export function App() {
       {isConnected && (
         <>
           <hr />
-          <h2 className="px-2">Read Contract</h2>
-          <GetOLMPricing />
+          <div className="flex flex-col m-2">
+            <label>OLM Address</label>
+            <input
+                onChange={(e) => setAddress(e.target.value)}
+                style={{marginLeft: 4}}
+                value={address}
+            />
+          </div>
+          <hr />
+          <h2 className="px-2">Rewards Estimator</h2>
+          <GetOLMPricing publicClient={publicClient} address={address}/>
           <hr />
           <h2 className="px-2">Exercise Widget</h2>
-          <ExerciseWidget />
+          <ExerciseWidget publicClient={publicClient} address={address}/>
         </>
       )}
     </div>
