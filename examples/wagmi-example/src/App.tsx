5e349484adbfce8ffb0456aba2d2b70526bfcb16
Oighty <oighty6@gmail.com>
2023-08-02 10:34:55 -0500

    deps: add tailwind + daisyui to example

--- examples/wagmi-example/src/App.tsx
+++ examples/wagmi-example/src/App.tsx
@@ -1,23 +1,31 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 import { GetOLMPricing } from './components/GetOLMPricing'
+import { ExerciseWidget } from './components/ExerciseWidget'
 
 export function App() {
   const { isConnected } = useAccount()
 
+  const oTokens = [
+    
+  ];
+
   return (
-    <>
-      <h1>wagmi + RainbowKit + Vite</h1>
+    <div className="prose">
+      <h1 className="p-2">oToken Front-end Example</h1>
 
       <ConnectButton />
 
       {isConnected && (
         <>
           <hr />
-          <h2>Read Contract</h2>
+          <h2 className="px-2">Read Contract</h2>
           <GetOLMPricing />
+          <hr />
+          <h2 className="px-2">Exercise Widget</h2>
+          <ExerciseWidget />
         </>
       )}
-    </>
+    </div>
   )
 }
