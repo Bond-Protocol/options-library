d8e49a7857c9974d46aa64604a41c5b2d93f4007
spaceturtleship <spaceturtleship@protonmail.com>
2023-08-03 17:54:23 +0100

    Enable ExerciseWidget on ethers example

--- examples/wagmi-ethers-example/src/App.tsx
+++ examples/wagmi-ethers-example/src/App.tsx
@@ -1,16 +1,17 @@
 import {ConnectButton} from '@rainbow-me/rainbowkit'
-import {useAccount, useNetwork, useProvider} from 'wagmi'
+import {useAccount, useNetwork, useProvider, useSigner} from 'wagmi'
 import {GetOLMPricing} from './components/GetOLMPricing'
-import {createPublicClient, http} from "viem";
+import {createPublicClient, createWalletClient, http} from "viem";
 import {GetInitializeBytecode} from "./components/GetInitializeBytecode";
 import {useState} from "react";
 import {ExerciseWidget} from "./components/ExerciseWidget";
 
 export function App() {
-    const {isConnected} = useAccount();
+    const {isConnected, address} = useAccount();
+    const {data: signer} = useSigner();
     const {chain} = useNetwork();
     const provider = useProvider();
-    const [address, setAddress] = useState<`0x${string}`>("0xb9fa19fc77fab92d90b0a010fbe7b22b045e5dd9");
+    const [olmAddress, setOlmAddress] = useState<`0x${string}`>("0xb9fa19fc77fab92d90b0a010fbe7b22b045e5dd9");
 
     const publicClient = createPublicClient({
             transport: http(provider.connection.url),
@@ -18,35 +19,47 @@ export function App() {
         }
     );
 
+    const walletClient = createWalletClient({
+            transport: http(signer?.provider.connection.url),
+            chain,
+            account: address
+        }
+    );
+
     return (
         <>
-            <h1>wagmi + RainbowKit + Vite</h1>
+            <h1>wagmi (legacy ethers version) + RainbowKit + Vite</h1>
 
             <ConnectButton/>
 
             {isConnected && (
                 <>
-                    <hr />
+                    <hr/>
                     <div className="flex flex-col m-2">
                         <label>OLM Address</label>
                         <input
-                            onChange={(e) => setAddress(e.target.value)}
+                            onChange={(e) => setOlmAddress(e.target.value)}
                             style={{marginLeft: 4}}
-                            value={address}
+                            value={olmAddress}
                         />
                     </div>
 
-                    <hr />
+                    <hr/>
                     <h2 className="px-2">Rewards Estimator</h2>
-                    <GetOLMPricing publicClient={publicClient} address={address}/>
+                    <GetOLMPricing publicClient={publicClient}
+                                   address={olmAddress}
+                    />
 
-                    <hr />
+                    <hr/>
                     <h2 className="px-2">Exercise Widget</h2>
-                    <ExerciseWidget publicClient={publicClient} address={address}/>
+                    <ExerciseWidget publicClient={publicClient}
+                                    walletClient={walletClient}
+                                    address={olmAddress}
+                    />
 
                     <hr/>
                     <h2>Get Initialize Bytecode</h2>
-                    <GetInitializeBytecode />
+                    <GetInitializeBytecode/>
                 </>
             )}
         </>
